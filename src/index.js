fbe6a8f0f5fa1fdcd9236f121b64f025ccdecb5a
datoprak <106121887+datoprak@users.noreply.github.com>
2023-04-22 15:01:52 +0300

    Get API data

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-alert("hello");
+const getAPIData = async () => {
+  const response = await fetch(
+    "https://api.openweathermap.org/data/2.5/weather?q=london&appid=09b3804c0eabfc48a079f18eaebc53b9&units=metric"
+  );
+  const data = await response.json();
+  console.log(data);
+};
+
+getAPIData();
